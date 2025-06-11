f165f70eba74a95521dbafe7d649135cddcf0ce8
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-11 19:15:50 +0530

    Update ManageCustomerRoutes.js

--- routes/ManageCustomerRoutes.js
+++ routes/ManageCustomerRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createManageCustomer, getAllManageCustomer } = require("../controllers/ManageCustomer");
+const { createManageCustomer, getAllManageCustomer, deleteManageCustomer } = require("../controllers/ManageCustomer");
 const authMiddleware = require("../Middleware/authMiddleware")
 
 router.post("/", authMiddleware, createManageCustomer);
 router.get("/", authMiddleware, getAllManageCustomer);
+router.delete("/:id", authMiddleware, deleteManageCustomer);
 
 module.exports = router;
