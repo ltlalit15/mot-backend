35b66da4b186b72a2f902a4a9262d407bfe60122
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-11 19:17:29 +0530

    Update leadMagnetRoutes.js

--- routes/leadMagnetRoutes.js
+++ routes/leadMagnetRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { createLeadMagnet, getAllLeadMagnets } = require("../controllers/leadMagnetController");
+const { createLeadMagnet, getAllLeadMagnets, editLeadMagnet, deleteLeadMagnet } = require("../controllers/leadMagnetController");
 const authMiddleware = require("../Middleware/authMiddleware")
 
 router.post("/", authMiddleware, createLeadMagnet);
 router.get("/", authMiddleware, getAllLeadMagnets);
+
+router.patch("/:id", authMiddleware, editLeadMagnet);
+router.delete("/:id", authMiddleware, deleteLeadMagnet);
+
+
+
 
 
 
