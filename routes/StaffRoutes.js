8e853f7350788ff58c00027d9b9d758de2d8b455
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-11 19:16:52 +0530

    Update StaffRoutes.js

--- routes/StaffRoutes.js
+++ routes/StaffRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createStaff, getAllStaff, editStaff } = require("../controllers/StaffController");
+const { createStaff, getAllStaff, editStaff, deleteStaff } = require("../controllers/StaffController");
 const authMiddleware = require("../Middleware/authMiddleware")
 
 router.post("/", authMiddleware, createStaff);
 router.get("/", authMiddleware, getAllStaff);
 router.patch("/:id", authMiddleware, editStaff);
+router.delete("/:id", authMiddleware, deleteStaff);
 module.exports = router;
