c1bb909441e47e47922849fb79e0b3fea8af2291
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-11 19:19:04 +0530

    Update GarageRoutes.js

--- routes/GarageRoutes.js
+++ routes/GarageRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {  createGarage ,  getAllGarages, editGarage }= require("../controllers/GarageControllers");
+const {  createGarage ,  getAllGarages, editGarage, deleteGarage }= require("../controllers/GarageControllers");
 const authMiddleware = require("../Middleware/authMiddleware")
 
 router.post('/', authMiddleware, createGarage)
 router.get('/', authMiddleware, getAllGarages)
-router.patch('/:id', authMiddleware, editGarage )
+router.patch('/:id', authMiddleware, editGarage)
 
-
+router.delete('/:id', authMiddleware, deleteGarage)
 
 module.exports = router;
