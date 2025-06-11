e29d548bd0b9c0f97046004c3bf9e2afdd8c0563
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-11 19:19:53 +0530

    Update ReviewRoutes.js

--- routes/ReviewRoutes.js
+++ routes/ReviewRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createReview, getAllReviews } = require("../controllers/ReviewControllers");
+const { createReview, getAllReviews, editReview, deleteReview  } = require("../controllers/ReviewControllers");
 const upload = require('../Middleware/Multer');
 const authMiddleware = require("../Middleware/authMiddleware")
 
 router.post("/", authMiddleware, createReview);
 router.get("/", authMiddleware, getAllReviews);
-
+router.patch("/:id", authMiddleware, editReview);
+router.delete("/:id", authMiddleware, deleteReview);
 module.exports = router;
