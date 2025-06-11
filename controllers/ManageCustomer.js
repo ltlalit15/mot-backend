048aa044f84d4e5857b88bc3e85619b6bfa031be
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-11 19:13:53 +0530

    Update ManageCustomer.js

--- controllers/ManageCustomer.js
+++ controllers/ManageCustomer.js
@@ -46,3 +46,31 @@ exports.getAllManageCustomer = async (req, res) => {
 
 
 
+
+exports.deleteManageCustomer = async (req, res) => {
+  try {
+    const customerId = req.params.id;
+
+    const deletedCustomer = await Customer.findByIdAndDelete(customerId);
+
+    if (!deletedCustomer) {
+      return res.status(404).json({
+        status: false,
+        message: "Customer not found"
+      });
+    }
+
+    res.status(200).json({
+      status: true,
+      message: "Customer deleted successfully",
+    
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: "Failed to delete customer",
+      error: error.message
+    });
+  }
+};
+
