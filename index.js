c90f439a9c26b696a0a79a2b8c5ccedc1ca66402
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-13 19:18:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ connectDB();
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5173", "https://mot-management.netlify.app/",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   //allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
