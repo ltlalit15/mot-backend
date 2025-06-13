cb36f487a6a89d399e932138c5990154cbec7263
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-13 19:20:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ connectDB();
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173", "https://mot-management.netlify.app/",
+  origin: ["http://localhost:5173", "https://mot-management.netlify.app/"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  //allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
+
 // ✅ Increase Payload Limit for Base64 Images
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
