ad2cd55c9f34d75f53b1842c71aa2fb909f58644
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-26 16:51:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -96,9 +96,11 @@ app.use("/api/vehicle", vehicleRoutes);
 const vehicleBulkRoutes = require("./routes/vehicleBulkRoutes");
 app.use("/api/vehicleBulk", vehicleBulkRoutes);
 
-
 const closedDayRoutes = require("./routes/closedDayRoutes");
 app.use("/api/closedDay", closedDayRoutes);
+
+const EmailTemplateRoutes = require("./routes/EmailTemplateRoutes");
+app.use("/api/EmailTemplate", EmailTemplateRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
