501d2a94ec3a67dcdf503fe3ab32f2171de64857
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-27 18:30:57 +0530

    Update ActivityRoutes.js

--- routes/ActivityRoutes.js
+++ routes/ActivityRoutes.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { logActivity, getAdminActivities }= require("../controllers/ActivityController");
 
-//const authMiddleware = require("../Middleware/authMiddleware")
+const authMiddleware = require("../Middleware/authMiddleware")
 
 router.post('/:logActivity', logActivity )
 
-router.get('/', getAdminActivities )
+router.get('/', authMiddleware, getAdminActivities )
 
 
 module.exports = router;
