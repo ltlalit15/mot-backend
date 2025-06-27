d3bb7999051debf027f4580400dabd301c4d9d15
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-27 18:30:34 +0530

    Update SystemRoutes.js

--- routes/SystemRoutes.js
+++ routes/SystemRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { getSystemData }= require("../controllers/SystemController");
 
-//const authMiddleware = require("../Middleware/authMiddleware")
+const authMiddleware = require("../Middleware/authMiddleware")
 
 
-router.get('/', getSystemData )
+router.get('/', authMiddleware, getSystemData )
 
 
 module.exports = router;
