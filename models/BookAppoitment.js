553a4758260b0773cfabf1af4c2e1a8c371e77ec
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-28 17:56:50 +0530

    Update BookAppoitment.js

--- models/BookAppoitment.js
+++ models/BookAppoitment.js
@@ -1,11 +1,8 @@
-
 const mongoose = require("mongoose");
 
 const bookAppoitSchema = new mongoose.Schema({
   vehicle: {
     vehicleRegistration: { type: String, required: true },
-    vinNumber: { type: String, required: true },
-    lastMotDate: { type: Date, required: true },
     make: { type: String, required: true },
     model: { type: String, required: true },
     year: { type: Number, required: true },
@@ -25,36 +22,13 @@ const bookAppoitSchema = new mongoose.Schema({
       repairs: { type: String }
     }]
   },
-  bookingSummary: {
-    additionalServices: [{
-      serviceName: { type: String },
-      price: { type: Number }
-    }],
-    costSummary: {
-      motTest: { type: Number, required: true },
-      additionalServices: { type: Number, required: true },
-      total: { type: Number, required: true }
-    }
-  },
   paymentDetails: {
-    reactivationFee: { type: Number, required: true },
-    administrativeProcessing: { type: Number, required: true },
-    vat: { type: Number, required: true },
-    total: { type: Number, required: true },
-    paymentMethod: { type: String, required: true },
-    paymentStatus: { type: String },
-    cardDetails: {
-      cardholderName: { type: String },
-      cardNumber: { type: String },
-      expiryDate: { type: String },
-      cvv: { type: String }
-    },
-    billingAddress: {
-      firstName: { type: String },
-      lastName: { type: String },
-      address: { type: String },
-      city: { type: String },
-      postcode: { type: String }
+    paymentMethod: {
+      fullName: { type: String, required: true },
+      emailAddress: { type: String, required: true },
+      phoneNumber: { type: String, required: true },
+      amount: { type: Number, required: true },
+      paymentPurpose: { type: String, required: true }
     }
   },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
