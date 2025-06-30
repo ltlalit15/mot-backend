14656cf28e26e44b8323cab476422e127572f101
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-30 15:47:37 +0530

    Update serviceRoutes.js

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { createService, getAllServices } = require("../controllers/serviceController");
+const { createService, getAllServices, updateService, deleteService } = require("../controllers/serviceController");
 const upload = require('../Middleware/Multer');
 const authMiddleware = require("../Middleware/authMiddleware")
 
 router.post("/", authMiddleware, createService);
 router.get("/", authMiddleware, getAllServices);
+router.patch("/:id", authMiddleware, updateService);
+router.delete("/:id", authMiddleware, deleteService);
+
+
 
 module.exports = router;
