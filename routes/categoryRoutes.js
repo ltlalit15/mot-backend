b36df6d0ff44b64fddcaeb69c64c43fa8eaad2b3
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-30 15:47:16 +0530

    Update categoryRoutes.js

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { createCategory,  getAllCategory } = require("../controllers/categoryController");
+const { createCategory, getAllCategory, updateCategory, deleteCategory } = require("../controllers/categoryController");
 
 router.post("/", createCategory);
 router.get("/", getAllCategory);
-
+router.patch("/:id", updateCategory);
+router.delete("/:id", deleteCategory);
 
 
 module.exports = router;
+
+
+
