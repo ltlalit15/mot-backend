83d206ecf49bf7cd589cebc4287892007b2c3243
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-07-02 18:28:25 +0530

    Update GarageUploadRoutes.js

--- routes/GarageUploadRoutes.js
+++ routes/GarageUploadRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createGarageUpload, getCombinedGarageData }= require("../controllers/GarageUploadController");
+const { createGarageUpload, getCombinedGarageData, getGarageData }= require("../controllers/GarageUploadController");
 const authMiddleware = require("../Middleware/authMiddleware")
 
 router.post('/', authMiddleware, createGarageUpload)
 router.get('/', authMiddleware, getCombinedGarageData)
+router.get('/getGarageData/:garageId/:place_id', authMiddleware, getGarageData)
 
 
 module.exports = router;
