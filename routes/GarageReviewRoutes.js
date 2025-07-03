78b46e9112d0a2c32fa39a44136764163234af34
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-07-03 13:38:33 +0530

    Update GarageReviewRoutes.js

--- routes/GarageReviewRoutes.js
+++ routes/GarageReviewRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createGarageReview, getAllGarageReview }= require("../controllers/GarageReviewController");
+const { createGarageReview, getAllGarageReview, getReviewsByGarageId }= require("../controllers/GarageReviewController");
 const authMiddleware = require("../Middleware/authMiddleware")
 
 router.post('/', authMiddleware, createGarageReview)
 router.get('/', authMiddleware, getAllGarageReview)
+router.get('/getReviewsByGarageId/:garageId', authMiddleware, getReviewsByGarageId)
 
 
 module.exports = router;
